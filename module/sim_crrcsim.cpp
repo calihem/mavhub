6db7ed576d4b131f5d1e63390a8c2f27b41b5b53
Oswald Berthold <oberthold@informatik.hu-berlin.de>
2010-12-21 10:27:31 +0100

    - now really added ctrl_bump and sim_crrcsim (.cpp and.h files)

---
+++ module/sim_crrcsim.cpp
@@ -0,0 +1,56 @@
+#include "sim_crrcsim.h"
+
+#include "logger.h"
+#include "utility.h"
+#include "protocolstack.h"
+#include <mavlink.h>
+
+#include <iostream> //cout
+using namespace std;
+
+namespace mavhub {
+Sim_Crrcsimule::Sim_Crrcsimule(const map<string, string> args) {
+	// initialize module parameters from conf
+	read_conf(args);
+	//app_id = 4;
+	//app_name = "crrcsim";
+}
+
+Sim_Crrcsimule::~Sim_Crrcsimule() {}
+
+void Sim_Crrcsimule::handle_input(const mavlink_message_t &msg) {
+	//Logger::log("Sim_Crrcsimule got mavlink_message, msgid:", (int)msg.msgid, (int)msg.len, Logger::LOGLEVEL_INFO);
+}
+
+void Sim_Crrcsimule::run() {
+	if(!owner) {
+		Logger::log("Owner of Sim_Crrcsimule not set", Logger::LOGLEVEL_WARN);
+		return;
+	}
+
+	int system_type = MAV_QUADROTOR;
+	mavlink_message_t msg_heartbeat;
+	mavlink_msg_heartbeat_pack(owner->system_id(), component_id, &msg_heartbeat, system_type, MAV_AUTOPILOT_GENERIC);
+
+	Logger::log("Sim_Crrcsimule started", Logger::LOGLEVEL_INFO);
+
+	while(1) {
+		//Logger::log("sim_crrcsim: system_id", static_cast<int>(owner->system_id()), Logger::LOGLEVEL_INFO);
+		send(msg_heartbeat);
+		sleep(1);
+	}
+}
+
+	void Sim_Crrcsimule::read_conf(const map<string, string> args) {
+		map<string,string>::const_iterator iter;
+		Logger::log("Sim_Crrcsimule::read_conf", Logger::LOGLEVEL_INFO);
+
+		iter = args.find("component_id");
+		if( iter != args.end() ) {
+			istringstream s(iter->second);
+			s >> component_id;
+		}
+		Logger::log("Sim_Crrcsimule::read_conf: component_id", component_id, Logger::LOGLEVEL_INFO);
+	}
+
+} // namespace mavhub
---
+++ module/sim_crrcsim.h
@@ -0,0 +1,31 @@
+#ifndef _SIM_CRRCSIM_H_
+#define _SIM_CRRCSIM_H_
+
+#include <inttypes.h> //uint8_t
+#include "protocollayer.h"
+
+namespace mavhub {
+
+	class Sim_Crrcsimule : public AppLayer {
+		public:
+			Sim_Crrcsimule(const std::map<std::string, std::string> args);
+			virtual ~Sim_Crrcsimule();
+			virtual void handle_input(const mavlink_message_t &msg);
+
+		protected:
+			virtual void run();
+
+	private:
+			uint16_t component_id;
+
+			virtual void read_conf(const std::map<std::string, std::string> args);
+	};
+	
+	// ----------------------------------------------------------------------------
+	// Sim_Crrcsimule
+	// ----------------------------------------------------------------------------
+
+
+} // namespace mavhub
+
+#endif
